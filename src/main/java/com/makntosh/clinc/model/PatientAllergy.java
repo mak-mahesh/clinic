362b7d9c474618104982a871e15dfab7f7a966f2
mak-mahesh <ma.kashalikar@gmail.com>
2026-06-20 17:41:54 +0530

    add patient allergy comment

--- src/main/java/com/makntosh/clinc/model/PatientAllergy.java
+++ src/main/java/com/makntosh/clinc/model/PatientAllergy.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
+/*Patient allergy info */
 @Entity
 @Table(name = "patient_allergy")
 public class PatientAllergy {
