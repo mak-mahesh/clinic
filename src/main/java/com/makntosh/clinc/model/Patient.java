5a305ca339811f8eb45e0047c0100040a5acb696
mak-mahesh <65345034+mak-mahesh@users.noreply.github.com>
2026-06-20 17:36:45 +0530

    Add comment for Patient class information
    Added a comment to indicate the purpose of the class.

--- src/main/java/com/makntosh/clinc/model/Patient.java
+++ src/main/java/com/makntosh/clinc/model/Patient.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+/* Patients informatino */
 @Entity
 @Table(name = "patient")
 public class Patient {
